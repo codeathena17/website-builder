033b053756e4d646d209c42dc4afc9712f954cac
Dheeraj Saraswat <dheeraj.saraswat@lmsathena.com>
2026-02-17 16:46:35 +0530

    chore(website-builder): initialize project with dependencies

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+function App() {
+  return (
+  <div className="text-5xl flex justify-center items-center font-bold text-center h-screen bg-gray-200">Website Builder</div>
+  );
+}
+
+export default App;
